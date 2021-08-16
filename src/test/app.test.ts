f85b7a7f64c5f8cdae4088124809e461a5e4d0f2
EvanSmith-git <EvanSmith-git@users.noreply.github.com>
2021-08-16 14:54:33 -0400

    Fix formatting in test

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -2,15 +2,14 @@ import { app } from '../app';
 import request = require('supertest');
 
 describe('App Test', () => {
-    it('should return status 200', async () => {
-        const res = await request(app).get('/');
-        expect(res.statusCode).toEqual(200);
-    });
-    
-    it('should say "Hello World!"', async () => {
-        const res = await request(app).get('/');
-        expect(res.statusCode).toEqual(200);
-        expect(res.text).toEqual('Hello World!');
-    });
-});
+  it('should return status 200', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+  });
 
+  it('should say "Hello World!"', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual('Hello World!');
+  });
+});
