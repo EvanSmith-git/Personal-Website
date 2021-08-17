b0304a9a0f25e369e7f8b51a30ebde0c3c9db509
EvanSmith-git <EvanSmith-git@users.noreply.github.com>
2021-08-16 20:48:59 -0400

    Sidebar, Font Awesome icons, test for sidebar

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -12,4 +12,12 @@ describe('App Test', () => {
     expect(res.statusCode).toEqual(200);
     expect(res.text).toContain('<title>Portfolio | Evan Smith</title>');
   });
+  
+  it('should have a sidebar', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toContain('<a href="#">About<i class="fas fa-info"></i></a>');
+    expect(res.text).toContain('<a href="#">Projects<i class="fas fa-code"></i></a>');
+    expect(res.text).toContain('<a href="#">Contact<i class="fas fa-address-card"></i></a>');
+  });
 });
